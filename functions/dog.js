cf4e3195c8b35f3a5c481f904b4304447306d451
sjoseph1848 <sjoseph1848@gmail.com>
2020-03-30 21:05:37 -0500

    separated into separate files, will continue

--- functions/dog.js
+++ functions/dog.js
@@ -1,22 +1,13 @@
 const functions = require('firebase-functions');
 const express = require('express')
 const admin = require('firebase-admin')
-admin.initializeApp();
 const request = require('request');
 const app = express();
 const cors = require('cors')
-const dog = require('./dog');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
 
-app.use(cors({origin:true}))
-// Test to get the api working, will refactor now that it works
-app.get('/doggos', (req,res) => {
+app.use(cors({ origin: true }));
+app.get('/puppers', (req,res) => {
     const options = {
         'method': 'POST',
         'url': 'https://api.petfinder.com/v2/oauth2/token?=',
@@ -48,6 +39,5 @@ app.get('/doggos', (req,res) => {
       });
 })
 
-exports.api = functions.https.onRequest(app);
+exports.dog = functions.https.onRequest(app);
 
-exports.dog = dog.dog
